07f80565d98916d162a6dc641c1ce9114fb1caa1
Vladimir Haiduk <nightshade2903@gmail.com>
2024-02-04 21:39:17 +0300

    start new develop

--- module-service/src/main/java/com/mjc/school/service/exception/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.mjc.school.service.exception;
-
-public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String msg){
-        super("ERROR_CODE: 400 ERROR_MESSAGE: " + msg);
-    }
-}
