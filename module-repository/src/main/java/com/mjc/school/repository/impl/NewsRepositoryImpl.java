c10c3fd0f7f959c8b894dc74faf6ac016be42c86
shade2903 <nightshade2903@gmail.com>
2024-02-06 16:27:47 +0300

    begin develop NewsRepository

---
+++ module-repository/src/main/java/com/mjc/school/repository/impl/NewsRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.mjc.school.repository.impl;
+
+import com.mjc.school.repository.NewsRepository;
+import com.mjc.school.repository.model.impl.NewsModel;
+
+import java.util.List;
+
+public class NewsRepositoryImpl implements NewsRepository<NewsModel> {
+    @Override
+    public NewsModel create(NewsModel entity) {
+        return null;
+    }
+
+    @Override
+    public List<NewsModel> readAll() {
+        return null;
+    }
+
+    @Override
+    public NewsModel readById(Long id) {
+        return null;
+    }
+
+    @Override
+    public NewsModel update(NewsModel entity) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
